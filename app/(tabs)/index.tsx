7caa3fe9dc069f8e67ea049e8d0991386a9a213a
sundaram26 <singhsundaram2004@gmail.com>
2026-04-01 07:51:44 +0530

    Font Configuration, Home page Header UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,43 +1,43 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 import { styled } from "nativewind";
+import images from "@/constants/images";
+import { icons } from "@/constants/icons";
+import { HOME_BALANCE, HOME_USER } from "@/constants/data";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from "dayjs";
 
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
     <SafeAreaView className="flex-1 p-5 bg-background">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link
-        href="/(auth)/sign-in"
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        Sign In
-      </Link>
-      <Link
-        href="/(auth)/sign-up"
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        Sign Up
-      </Link>
-      <Link
-        href="/subscriptions/spotify"
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        Spotify Subscription
-      </Link>
-      <Link
-        href={{
-          pathname: "/subscriptions/[id]",
-          params: { id: "claude" }
-        }}
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        Claude Max Subscription
-      </Link>
+      <View className="home-header">
+        <View className="home-user">
+          <Image
+            source={images.avatar}
+            className="home-avatar"
+          />
+          <Text className="home-user-name">{HOME_USER.name}</Text>
+        </View>
+
+        <Image
+          source={icons.add}
+          className="home-add-icon"
+        />
+      </View>
+      <View className="home-balance-card">
+        <Text className="home-balance-label">Balance</Text>
+        <View className="home-balance-row">
+          <Text className="home-balance-amount">
+            {formatCurrency(HOME_BALANCE.amount)}
+          </Text>
+          <Text className="home-balance-date">
+            {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD")}
+          </Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
